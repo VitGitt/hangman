d272d63fb37f04b14d49e0ad25dc478c697c027a
root <root@DSKLNV-WRK13.localdomain>
2020-05-12 20:18:52 +0300

    first commit

---
+++ testt.py
@@ -0,0 +1,18 @@
+#def f(a):
+#    return a**2
+#print(f(8) + f(910))
+
+#def g(s):
+#    return s + 5
+#print(g(f(3)))
+#print(len("Men"))
+#len("gihdsfgjhsdgjkhjkgf")
+#print(len(str(5989)))
+#print(float(8))
+
+age = input("укажите возраст")
+int_age = int(age)
+if int_age <= 21:
+    print("Вы - молоды")
+else:
+    print('Вы - старичок')
